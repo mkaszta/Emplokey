2ca21b4387580b4ae47cb235dee60bf09357a094
mkaszta <michal.kaszta@gmail.com>
2015-11-04 14:47:57 +0100

    4/11/2015 - server app changes

---
+++ Emplokey Server/Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emplokey_Server
+{
+    public class Server
+    {
+        public string address { get; set; }
+        public string dbName { get; set; }
+        public string userName { get; set; }
+        public string isDefault { get; set; }
+    }
+}
--- Emplokey Server/form_main.cs
+++ Emplokey Server/form_main.cs
@@ -14,20 +14,62 @@ namespace Emplokey_Server
     public partial class Form_main : Form
     {
         DbManager dbMgr = new DbManager();
+        public List<Server> serverList = new List<Server>();
 
         public Form_main()
         {
             InitializeComponent();
+            updateComboBox();
+        }
+
+        private void btnNewDB_Click(object sender, EventArgs e)
+        {
+            string DbServer = textBoxAddress.Text;
+            string DbName = textBoxDbName.Text;
+            string Username = textBoxUsername.Text;
+            string Password = textBoxPassword.Text;
+
+            try
+            {
+                dbMgr.createDatabase(DbServer, DbName, Username, Password);
+                updateComboBox();               
+            }            
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }        
+
+        private void btnSetDefault_Click(object sender, EventArgs e)
+        {
+            dbMgr.setDefaultServer(textBoxAddress.Text);
+        }
+
+        private void comboBoxServers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBoxAddress.Text = serverList[comboBoxServers.SelectedIndex].address;
+            textBoxDbName.Text = serverList[comboBoxServers.SelectedIndex].dbName;
+            textBoxUsername.Text = serverList[comboBoxServers.SelectedIndex].userName;
+            textBoxPassword.Text = "";
+            btnConnect.Enabled = true;
+        }
+
+        private void updateComboBox()
+        {
+            comboBoxServers.Items.Clear();
+            dbMgr.getServersList(this);
+
+            foreach (var server in serverList)
+            {
+                comboBoxServers.Items.Add(server.address + " (" + server.dbName + ")");
+                if (server.isDefault == "true")
+                    comboBoxServers.SelectedIndex = comboBoxServers.Items.Count - 1;
+            }
         }
 
         private void btnConnect_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void btnCreate_Click(object sender, EventArgs e)
-        {
-            dbMgr.createDatabase();
+            dbMgr.connect(serverList[comboBoxServers.SelectedIndex]);
         }
     }
 }
